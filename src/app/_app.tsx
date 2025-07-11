1cc7955980b0074969eeebecf460634b51b6caee
gyubin0128 <rachel57bin@gmail.com>
2025-07-12 02:06:21 +0900

    feat: add Geist UI and setup _app.tsx

---
+++ src/app/_app.tsx
@@ -0,0 +1,13 @@
+import { GeistProvider, CssBaseline } from '@geist-ui/react'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  )
+}
+
+export default MyApp 
